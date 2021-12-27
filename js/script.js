72d839d51696c1b5135171c0827a2b67dfaa9ee8
! レム <80942588+rem-9999@users.noreply.github.com>
2021-12-27 11:19:00 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
-var images = ['https://i.imgur.com/mA8yda8.jpg', 'https://imgur.com/Hsw4nhm.png'];
+var images = ['https://i.imgur.com/vasZSd2.jpeg'];
 
 $('#container').append('<style>#container, .acceptContainer:before, .logoContainer:before {background: url(' + images[Math.floor(Math.random() * images.length)] + ') center fixed }');
